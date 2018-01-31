a2ece089c2c9172ce7bf91cd3a7b8f7b91587513
Idanord <33676867+Idanord@users.noreply.github.com>
2018-01-31 09:19:39 -0800

    Added code for edit text view (fahrenheitEditText, celsiusEditText)

--- app/src/main/java/com/fisher/tempconverter/MainActivity.java
+++ app/src/main/java/com/fisher/tempconverter/MainActivity.java
@@ -2,14 +2,34 @@ package com.fisher.tempconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare variables
+    private EditText fahrenhietEditText;
+    private EditText celciusEditText;
+    private TextView convertionTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get references
+        fahrenhietEditText = (EditText) findViewById(R.id.fahrenheitEditText);
+        celciusEditText = (EditText) findViewById(R.id.celsiusEditView);
+        convertionTextView = (TextView)findViewById(R.id.convertionTextView);
+
     }
 
-    //comment to upload
+    public static float convertFahenheitToCelsius(float fahrenheit){
+        return ((fahrenheit - 32)* 5/9);
+    }
+
+    public static float convertCelsiusToFahengeit(float celsius){
+        return ((celsius * 9)/5) + 32;
+    }
 }
